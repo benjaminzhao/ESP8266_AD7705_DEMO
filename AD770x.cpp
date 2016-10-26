81be47b9062885a77a2c6d8c4311d4ad708d4772
benjaminzhao <benjamin.zhao.1983@gmail.com>
2016-10-26 13:37:21 -0400

    Add files via upload

---
+++ AD770x.cpp
@@ -0,0 +1,150 @@
+/*
+ * Benjamin Zhao
+ * Modified for ESP8266 Module
+ * Using SPI Library
+ * version 1.0
+ * Date: 2016-10
+ * Description: (a) Using Arduino SPI Library
+ *              (b) change constructor AD770X()
+ *              (c) add funtion resetHard() by uisng RESET pin
+ *              (d) replace spiTransfer() with SPI.transfer()
+ *              (e) setup ESP8266 SPI to Mode2 & MSBFIRST & 1 MHz
+ *
+ * Orignal Source Code by:
+ * AD7705/AD7706 Library
+ * Kerry D. Wong
+ * http://www.kerrywong.com
+ * Initial version 1.0 3/2011
+ * Updated 1.1 4/2012
+ */
+
+#include "AD770X.h"
+
+//write communication register
+//   7        6      5      4      3      2      1      0
+//0/DRDY(0) RS2(0) RS1(0) RS0(0) R/W(0) STBY(0) CH1(0) CH0(0)
+
+void AD770X::setNextOperation(byte reg, byte channel, byte readWrite) {
+    byte r = 0;
+    r = reg << 4 | readWrite << 3 | channel;
+
+    digitalWrite(pinCS, LOW);
+    SPI.transfer(r);
+    digitalWrite(pinCS, HIGH);
+}
+
+//Clock Register
+//   7      6       5        4        3        2      1      0
+//ZERO(0) ZERO(0) ZERO(0) CLKDIS(0) CLKDIV(0) CLK(1) FS1(0) FS0(1)
+//
+//CLKDIS: master clock disable bit
+//CLKDIV: clock divider bit
+
+void AD770X::writeClockRegister(byte CLKDIS, byte CLKDIV, byte outputUpdateRate) {
+    byte r = CLKDIS << 4 | CLKDIV << 3 | outputUpdateRate;
+
+    r &= ~(1 << 2); // clear CLK
+
+    digitalWrite(pinCS, LOW);
+    SPI.transfer(r);
+    digitalWrite(pinCS, HIGH);
+}
+
+//Setup Register
+//  7     6     5     4     3      2      1      0
+//MD10) MD0(0) G2(0) G1(0) G0(0) B/U(0) BUF(0) FSYNC(1)
+
+void AD770X::writeSetupRegister(byte operationMode, byte gain, byte unipolar, byte buffered, byte fsync) {
+    byte r = operationMode << 6 | gain << 3 | unipolar << 2 | buffered << 1 | fsync;
+
+    digitalWrite(pinCS, LOW);
+    SPI.transfer(r);
+    digitalWrite(pinCS, HIGH);
+}
+
+unsigned int AD770X::readADResult() {
+    digitalWrite(pinCS, LOW);
+    byte b1 = SPI.transfer(0x0);
+    byte b2 = SPI.transfer(0x0);
+    digitalWrite(pinCS, HIGH);
+
+    unsigned int r = b1 << 8 | b2;
+
+    return r;
+}
+
+unsigned int AD770X::readADResultRaw(byte channel) {
+    while (!dataReady(channel)) {
+    };
+    setNextOperation(REG_DATA, channel, 1);
+
+    return readADResult();
+}
+
+double AD770X::readADResult(byte channel, float refOffset) {
+    return readADResultRaw(channel) * 1.0 / 65536.0 * VRef - refOffset;
+}
+
+bool AD770X::dataReady(byte channel) {
+    setNextOperation(REG_CMM, channel, 1);
+
+    digitalWrite(pinCS, LOW);
+    byte b1 = SPI.transfer(0x0);
+    digitalWrite(pinCS, HIGH);
+
+    return (b1 & 0x80) == 0x0;
+}
+
+void AD770X::reset() {
+    digitalWrite(pinCS, LOW);
+    for (int i = 0; i < 10; i++){
+        SPI.transfer(0xff);
+    }
+        
+    digitalWrite(pinCS, HIGH);
+}
+
+AD770X::AD770X(double vref, int _pinCS, int _pinMOSI, int _pinMISO, int _pinSPIClock, int _pinRST) {
+    VRef = vref;
+    pinMOSI = _pinMOSI;
+    pinMISO = _pinMISO;
+    pinSPIClock = _pinSPIClock;
+    pinCS = _pinCS;
+    pinRST = _pinRST;
+    
+    pinMode(pinMOSI, OUTPUT);
+    pinMode(pinMISO, INPUT);
+    pinMode(pinSPIClock, OUTPUT);
+    pinMode(pinCS, OUTPUT);
+    pinMode(pinRST, OUTPUT);
+
+    SPI.begin();
+    SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE2));
+    
+    digitalWrite(pinCS, HIGH);
+}
+
+void AD770X::init(byte channel, byte clkDivider, byte polarity, byte gain, byte updRate) {
+    setNextOperation(REG_CLOCK, channel, 0);
+    writeClockRegister(0, clkDivider, updRate);
+
+    setNextOperation(REG_SETUP, channel, 0);
+    writeSetupRegister(MODE_SELF_CAL, gain, polarity, 0, 0);
+
+    while (!dataReady(channel)) {
+    };
+}
+
+void AD770X::init(byte channel) {
+    init(channel, CLK_DIV_1, BIPOLAR, GAIN_1, UPDATE_RATE_25);
+}
+
+void AD770X::resetHard(){
+    digitalWrite(pinRST, HIGH);
+	delay(1);
+	digitalWrite(pinRST, LOW);
+	delay(2);
+	digitalWrite(pinRST, HIGH);
+	delay(1);
+}
+
---
+++ esp8266_SPI_7705.ino
@@ -0,0 +1,78 @@
+#include <AD770X.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiAP.h>
+#include <ESP8266WiFiGeneric.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266WiFiScan.h>
+#include <ESP8266WiFiSTA.h>
+#include <ESP8266WiFiType.h>
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
+#include <WiFiServer.h>
+#include <WiFiUdp.h>
+
+// Connect AD7705 using SPI
+// ESP8266 -- NodeMCU -> Pin
+// GPIO15  --   D8    -> CS
+// GPIO13  --   D7    -> MOSI
+// GPIO12  --   D6    -> MISO
+// GPIO14  --   D5    -> SCLK
+// GPIO05  --   D1    -> RESET hard
+
+// Example based on NodeMCU
+AD770X TM7705(2.5, D8, D7, D6, D5, D1);
+
+
+
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(74880);    // set to ESP8266 bootloader baudrate, so that you can see the boot info
+
+    TM7705.resetHard();
+    TM7705.reset();
+    
+    Serial.println();
+    Serial.println("--Start--");
+    
+    TM7705.init(AD770X::CHN_AIN1, AD770X::CLK_DIV_1, AD770X::BIPOLAR, AD770X::GAIN_4, AD770X::UPDATE_RATE_50);
+    TM7705.init(AD770X::CHN_AIN2, AD770X::CLK_DIV_1, AD770X::BIPOLAR, AD770X::GAIN_4, AD770X::UPDATE_RATE_50);
+    Serial.println("--Init Done--");
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    static int i = 0;
+    double v1 = 0.0;
+    double v2 = 0.0;
+    
+    Serial.print("#");
+    Serial.print(i++);
+    Serial.print(": ");
+    
+    //read twice to improve reliability
+    Serial.print("CH1: ");
+    while (!TM7705.dataReady(AD770X::CHN_AIN1)) {
+        ;
+    }
+    v1 = TM7705.readADResult(AD770X::CHN_AIN1, 1.25); // ref offest = 125
+    while (!TM7705.dataReady(AD770X::CHN_AIN1)) {
+        ;
+    }
+    v1 = TM7705.readADResult(AD770X::CHN_AIN1, 1.25);
+    Serial.print(v1, 5);
+    
+    //read twice to improve reliability
+    Serial.print("  CH2: ");
+    while (!TM7705.dataReady(AD770X::CHN_AIN2)) {
+        ;
+    }
+    v2 = TM7705.readADResult(AD770X::CHN_AIN2, 1.25);
+    while (!TM7705.dataReady(AD770X::CHN_AIN2)) {
+        ;
+    }
+    v2 = TM7705.readADResult(AD770X::CHN_AIN2, 1.25);
+    Serial.println(v2, 5);
+    
+    delay(1000);
+}
